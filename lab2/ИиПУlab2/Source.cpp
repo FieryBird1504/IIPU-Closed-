a93d37dd0374e5c142edbde6f558ea54106e494b
Fiery Bird <alex.d150498@gmail.com>
2017-09-29 13:04:02 +0300

    Добавил функцию получения данных о свободном и занятом месте на дисках

--- lab2/ИиПУlab2/Source.cpp
+++ lab2/ИиПУlab2/Source.cpp
@@ -13,7 +13,48 @@ using namespace std;
 char* busType[] = { "UNKNOWN", "SCSI", "ATAPI", "ATA", "ONE_TREE_NINE_FOUR", "SSA", "FIBRE", "USB", "RAID", "ISCSI", "SAS", "SATA", "SD", "MMC" };
 char* driveType[] = { "UNKNOWN", "INVALID", "CARD_READER/FLASH", "HARD", "REMOTE", "CD_ROM", "RAM" };
 
+void getMemoryInfo() {
+	string path;
+	_ULARGE_INTEGER diskSpace;
+	_ULARGE_INTEGER freeSpace;
 
+	diskSpace.QuadPart = 0;
+	freeSpace.QuadPart = 0;
+
+	_ULARGE_INTEGER totalDiskSpace;
+	_ULARGE_INTEGER totalFreeSpace;
+	totalDiskSpace.QuadPart = 0;
+	totalFreeSpace.QuadPart = 0;
+
+	unsigned long int logicalDrivesCount = GetLogicalDrives();
+	cout.setf(ios::left);
+	cout << setw(8) << "Disk"
+		<< setw(16) << "Total space[Mb]"
+		<< setw(16) << "Free space[Mb]"
+		<< setw(16) << "Busy space[%]"
+		<< setw(16) << "Driver type"
+		<< endl;
+
+	for (char var = 'A'; var < 'Z'; var++) {
+		if ((logicalDrivesCount >> var - 65) & 1) {
+			path = var;
+			path.append(":\\");
+			GetDiskFreeSpaceEx(path.c_str(), 0, &diskSpace, &freeSpace);
+			diskSpace.QuadPart = diskSpace.QuadPart / (bThousand * bThousand);
+			freeSpace.QuadPart = freeSpace.QuadPart / (bThousand * bThousand);
+
+			cout << setw(8) << var
+				<< setw(16) << diskSpace.QuadPart
+				<< setw(16) << freeSpace.QuadPart
+				<< setw(16) << setprecision(3) << 100.0 - (double)freeSpace.QuadPart / (double)diskSpace.QuadPart * Hundred
+				<< setw(16) << driveType[GetDriveType(path.c_str())]
+				<< endl;
+			if (GetDriveType(path.c_str()) == 3) {
+				totalDiskSpace.QuadPart += diskSpace.QuadPart;
+				totalFreeSpace.QuadPart += freeSpace.QuadPart;
+			}
+		}
+	}
 
 int main() {
 	STORAGE_PROPERTY_QUERY storageProtertyQuery;
