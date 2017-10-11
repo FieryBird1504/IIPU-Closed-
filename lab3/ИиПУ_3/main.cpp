a9b744a3fdf3c3df8fabc9f8c4bbe74530c623b1
Fiery Bird <alex.d150498@gmail.com>
2017-10-11 17:15:13 +0300

    Написал функции получения статуса подключения (сеть/батарея) , времени работы , оставшегося уровня заряда в процентах.

--- lab3/ИиПУ_3/main.cpp
+++ lab3/ИиПУ_3/main.cpp
@@ -8,14 +8,26 @@ extern "C"
 {
 	__declspec(dllexport) int StatusCheck()
 	{
+		SYSTEM_POWER_STATUS system_power_status;
+		ZeroMemory(&system_power_status, sizeof(SYSTEM_POWER_STATUS));
+		GetSystemPowerStatus(&system_power_status);
+		return system_power_status.ACLineStatus;
 	}
 
 	__declspec(dllexport) int RemainTime()
 	{
+		SYSTEM_POWER_STATUS system_power_status;
+		ZeroMemory(&system_power_status, sizeof(SYSTEM_POWER_STATUS));
+		GetSystemPowerStatus(&system_power_status);
+		return system_power_status.BatteryLifeTime;
 	}
 
 	__declspec(dllexport) int RemainPercent()
 	{
+		SYSTEM_POWER_STATUS system_power_status;
+		ZeroMemory(&system_power_status, sizeof(SYSTEM_POWER_STATUS));
+		GetSystemPowerStatus(&system_power_status);
+		return (system_power_status.BatteryLifePercent);
 	}
 
 	__declspec(dllexport) void SetTimeout(int new_timeout)
