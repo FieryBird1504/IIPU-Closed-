e7f52ea4dd3c83739cc2da04fba93c9d4d74fd3b
Fiery Bird <alex.d150498@gmail.com>
2017-10-20 14:06:22 +0300

    Начал четвёртую лабораторную. Добавил класс , отвечающий за извлечение устройства.

---
+++ lab4/USBMonitorInForms/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using PortableDeviceApiLib;
+using System.Threading;
+
+namespace USBMonitorInForms
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_DeviceChange = 0x219;          // something happened to usb
+        private const int DBT_DEVICEARRIVAL = 0x8000;       // device connected
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;// device disconnected
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadInfo();
+        }
+    }
+}
---
+++ lab4/USBMonitorInForms/MTPDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PortableDeviceApiLib;
+using PortableDeviceTypesLib;
+using _tagpropertykey = PortableDeviceApiLib._tagpropertykey;
+using IPortableDeviceKeyCollection = PortableDeviceApiLib.IPortableDeviceKeyCollection;
+using IPortableDeviceValues = PortableDeviceApiLib.IPortableDeviceValues;
+
+namespace USBMonitorInForms
+{
+    public class MTPDevice
+    {
+    }
+}
