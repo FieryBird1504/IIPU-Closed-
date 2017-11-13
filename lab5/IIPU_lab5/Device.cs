d186be1b4aaeb7879bfc4ad874843e4844d28b27
Fiery Bird <alex.d150498@gmail.com>
2017-11-13 21:17:29 +0300

    Дописал функцию включения/отключения.

--- lab5/IIPU_lab5/Device.cs
+++ lab5/IIPU_lab5/Device.cs
@@ -40,5 +40,28 @@ namespace IIPU_lab4_GUI
 
         public string GetSysFilePath(int index)
         { return SysFiles[index].PathName; }
+
+        public void ToggleEnable()
+        {
+            try
+            {
+                var device = new ManagementObjectSearcher("SELECT * FROM Win32_PNPEntity").Get()
+                    .OfType<ManagementObject>()
+                    .FirstOrDefault(x => x.Properties["DeviceID"].Value.ToString().Equals(DeviceID));
+
+                if (Enabled)
+                {
+                    device.InvokeMethod("Disable", new object[] { false });
+                }
+                else
+                {
+                    device.InvokeMethod("Enable", new object[] { false });
+                }
+
+                Enabled = !Enabled;
+            }
+            catch (Exception)
+            { MessageBox.Show("Cannot disconnect the device!"); }
+        }
     }
 }
