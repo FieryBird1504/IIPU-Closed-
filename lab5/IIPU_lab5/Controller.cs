2475a7f6e368ac2cd7c82c39dbf49aa6400cd49e
Fiery Bird <alex.d150498@gmail.com>
2017-11-13 19:09:32 +0300

    Начал пятую лабораторную , для GUI сделал вывод списка устройств и информации о них.

---
+++ lab5/IIPU_lab5/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Management; 
+
+namespace IIPU_lab4_GUI
+{
+	
+	class Controller
+	{
+
+	}
+}
---
+++ lab5/IIPU_lab5/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Windows.Forms;
+
+namespace IIPU_lab4_GUI
+{
+	public class Device
+	{
+
+	}
+}
