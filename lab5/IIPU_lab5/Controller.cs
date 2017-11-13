8db08d0dc9096a85b7907cd36ef97b9e1b943325
Fiery Bird <alex.d150498@gmail.com>
2017-11-13 19:47:24 +0300

    Добавил класс контроллер , написал в нём функции получения и обновления информации.

--- lab5/IIPU_lab5/Controller.cs
+++ lab5/IIPU_lab5/Controller.cs
@@ -12,6 +12,59 @@ namespace IIPU_lab4_GUI
 	
 	class Controller
 	{
+        Form1 form;
+        //Timer updateTimer;
+        public List<Device> deviceList = new List<Device>();
 
-	}
-}
+
+        public Controller(Form1 formArg)
+        {
+            form = formArg;
+            /*
+			updateTimer = new Timer();
+			updateTimer.Interval = 1000; //ms
+			updateTimer.Enabled = true;
+			updateTimer.Tick += UpdateUsbInfo;
+			*/
+            UpdateUsbInfo(null, null);
+        }
+
+
+        private void UpdateUsbInfo(object sender, EventArgs e)
+        {
+            //deviceList = newDeviceList;
+
+            form.cb_List.Items.Clear();
+
+
+
+            deviceList = new List<Device>();
+            var devices = new ManagementObjectSearcher("SELECT * FROM Win32_PNPEntity");
+
+
+
+
+            foreach (ManagementObject devObj in devices.Get())
+            {
+
+                var device = new Device
+                {
+                    Name = devObj["Name"] != null ? devObj["Name"].ToString() : "",
+                    ClassGuid = devObj["ClassGuid"] != null ? devObj["ClassGuid"].ToString() : "",
+                    HardwareID = devObj["HardwareID"] != null ? (string[])devObj["HardwareID"] : null,
+                    Manufacturer = devObj["Manufacturer"] != null ? devObj["Manufacturer"].ToString() : "",
+                    DeviceID = devObj["DeviceID"] != null ? devObj["DeviceID"].ToString() : "",
+                    Enabled = devObj["Status"].ToString() == "OK"
+                };
+
+
+
+                foreach (var sys in devObj.GetRelated("Win32_SystemDriver"))
+                {
+                    device.AddSysFile(sys["PathName"].ToString(), sys["Description"].ToString());
+                }
+                deviceList.Add(device);
+            }
+
+        }
+    }
