4ae81bc1d47e2458f1cae99e6c35f6703e97fe53
Fiery Bird <alex.d150498@gmail.com>
2017-11-13 21:08:13 +0300

    Добавил класс Device , возвращающий информацию об устройстве.

--- lab5/IIPU_lab5/Device.cs
+++ lab5/IIPU_lab5/Device.cs
@@ -10,6 +10,35 @@ namespace IIPU_lab4_GUI
 {
 	public class Device
 	{
+        public string Name,
+                                    ClassGuid,
+                                    Manufacturer,
+                                    DeviceID;
+        public string[] HardwareID;
+        private List<SysFile> SysFiles = new List<SysFile>();
+        public bool Enabled;
 
-	}
+        class SysFile
+        {
+            public string PathName;
+            public string Description;
+        }
+
+        public void AddSysFile(string pname, string desc)
+        {
+            var sysFile = new SysFile();
+            sysFile.PathName = pname;
+            sysFile.Description = desc;
+            SysFiles.Add(sysFile);
+        }
+
+        public int GetSysFileCount()
+        { return SysFiles.Count(); }
+
+        public string GetSysFileDesc(int index)
+        { return SysFiles[index].Description; }
+
+        public string GetSysFilePath(int index)
+        { return SysFiles[index].PathName; }
+    }
 }
