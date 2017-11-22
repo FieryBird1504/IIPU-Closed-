f7332c2f0a51aaa5b4ca3dec1c4317eaeaf736c5
Fiery Bird <alex.d150498@gmail.com>
2017-11-22 12:55:54 +0300

    Добавил получение данных о диске в класс DiscData.

--- lab7/burn_sharp_forms/BurnData.cs
+++ lab7/burn_sharp_forms/BurnData.cs
@@ -8,6 +8,21 @@
 
     public class BurnData
     {
-        
+        public string uniqueRecorderId;
+        public string statusMessage;
+        public string volumeName;
+        public BURN_MEDIA_TASK task;
+
+        public long elapsedTime;
+        public long remainingTime;
+        public long totalTime;
+        public IMAPI_FORMAT2_DATA_WRITE_ACTION currentAction;
+        public long startLba;
+        public long sectorCount;
+        public long lastReadLba;
+        public long lastWrittenLba;
+        public long totalSystemBuffer;
+        public long usedSystemBuffer;
+        public long freeSystemBuffer;
     }
 }
--- lab7/burn_sharp_forms/DiscData.cs
+++ lab7/burn_sharp_forms/DiscData.cs
@@ -9,6 +9,140 @@ namespace burn_sharp_forms
 {
     public class DiscData
     {
-        
+        public static string GetMediaTypeString(IMAPI_MEDIA_PHYSICAL_TYPE mediaType)
+        {
+            switch (mediaType)
+            {
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_UNKNOWN:
+                default:
+                    return "Unknown Media Type";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_CDROM:
+                    return "CD-ROM";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_CDR:
+                    return "CD-R";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_CDRW:
+                    return "CD-RW";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDROM:
+                    return "DVD ROM";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDRAM:
+                    return "DVD-RAM";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDPLUSR:
+                    return "DVD+R";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDPLUSRW:
+                    return "DVD+RW";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDPLUSR_DUALLAYER:
+                    return "DVD+R Dual Layer";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDDASHR:
+                    return "DVD-R";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDDASHRW:
+                    return "DVD-RW";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDDASHR_DUALLAYER:
+                    return "DVD-R Dual Layer";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DISK:
+                    return "random-access writes";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDPLUSRW_DUALLAYER:
+                    return "DVD+RW DL";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_HDDVDROM:
+                    return "HD DVD-ROM";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_HDDVDR:
+                    return "HD DVD-R";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_HDDVDRAM:
+                    return "HD DVD-RAM";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_BDROM:
+                    return "Blu-ray DVD (BD-ROM)";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_BDR:
+                    return "Blu-ray media";
+
+                case IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_BDRE:
+                    return "Blu-ray Rewritable media";
+            }
+        }
+
+        public static string GetProfileTypeString(IMAPI_PROFILE_TYPE profileType)
+        {
+            switch (profileType)
+            {
+                default:
+                    return string.Empty;
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_CD_RECORDABLE:
+                    return "CD-R";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_CD_REWRITABLE:
+                    return "CD-RW";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVDROM:
+                    return "DVD ROM";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_DASH_RECORDABLE:
+                    return "DVD-R";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_RAM:
+                    return "DVD-RAM";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_PLUS_R:
+                    return "DVD+R";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_PLUS_RW:
+                    return "DVD+RW";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_PLUS_R_DUAL:
+                    return "DVD+R Dual Layer";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_DASH_REWRITABLE:
+                    return "DVD-RW";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_DASH_RW_SEQUENTIAL:
+                    return "DVD-RW Sequential";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_SEQUENTIAL:
+                    return "DVD-R DL Sequential";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_LAYER_JUMP:
+                    return "DVD-R Dual Layer";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_DVD_PLUS_RW_DUAL:
+                    return "DVD+RW DL";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_HD_DVD_ROM:
+                    return "HD DVD-ROM";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_HD_DVD_RECORDABLE:
+                    return "HD DVD-R";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_HD_DVD_RAM:
+                    return "HD DVD-RAM";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_BD_ROM:
+                    return "Blu-ray DVD (BD-ROM)";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_BD_R_SEQUENTIAL:
+                    return "Blu-ray media Sequential";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_BD_R_RANDOM_RECORDING:
+                    return "Blu-ray media";
+
+                case IMAPI_PROFILE_TYPE.IMAPI_PROFILE_TYPE_BD_REWRITABLE:
+                    return "Blu-ray Rewritable media";
+            }
+        }
     }
 }
